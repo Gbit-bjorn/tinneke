ecc89bc33e16c3753b416965a1e9414a89169f72
Gbit-bjorn <149739722+Gbit-bjorn@users.noreply.github.com>
2026-03-31 07:37:46 +0200

    rework

--- blueprints/export/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-export_bp = Blueprint(
-    "export",
-    __name__,
-    url_prefix="/export",
-)
-
-from blueprints.export import routes  # noqa: E402, F401
--- blueprints/klassen/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-klassen_bp = Blueprint("klassen", __name__, url_prefix="/klassen")
-
-from blueprints.klassen import routes  # noqa: E402, F401
